af1f4c9c8d64e8e84c1c49ed2f594868461f83a9
ilyazt04 <azroutilyass@gmail.com>
2026-08-12 00:25:29 +0100

    Prepare Django for Render deployment

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -29,10 +29,8 @@ environ.Env.read_env(BASE_DIR / '.env')
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
 # Application definition
 
