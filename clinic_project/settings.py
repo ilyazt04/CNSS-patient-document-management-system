01e165d6bcd05580dd4df59fe32b32db4eb085f1
ilyazt04 <azroutilyass@gmail.com>
2026-08-12 10:32:41 +0100

    settings config

--- clinic_project/settings.py
+++ clinic_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 # SECURITY WARNING: don't run with debug turned on in production!
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
